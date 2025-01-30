f27fd2fece3f030c4da8d2d1919d2be1f9454e40
Thomas <hi@itcon.au>
2025-01-31 00:47:40 +1100

    Update s3.js

--- s3.js
+++ s3.js
@@ -1288,7 +1288,7 @@ async function importFromS3() {
 			const k = 1024;
 			const sizes = ['Bytes', 'KB', 'MB', 'GB'];
 			const i = Math.floor(Math.log(bytes) / Math.log(k));
-			return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]);
+			return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 		};
 
 		// Calculate size difference percentage
