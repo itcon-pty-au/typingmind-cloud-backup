1563eefb86abeb857561dcb2db102385e2d921b5
Thomas <hi@itcon.au>
2025-01-31 15:47:59 +1100

    Update s3.js

--- s3.js
+++ s3.js
@@ -220,7 +220,15 @@ function openSyncModal() {
 				    <input id="backup-interval" name="backup-interval" type="number" min="30" class="z-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-zinc-700" autocomplete="off" required>
 				</div>
                                 <div>
-                                    <label for="encryption-key" class="block text-sm font-medium text-gray-700 dark:text-gray-400">Encryption Key</label>
+                                    <label for="encryption-key" class="block text-sm font-medium text-gray-700 dark:text-gray-400">
+                                        Encryption Key
+                                        <span class="ml-1 relative group cursor-pointer">
+                                            <span class="text-xs">ℹ</span>
+                                            <div class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 p-2 w-64 bg-black text-white text-xs rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-10">
+                                                Choose a secure 8+ character string. This is to encrypt the backup file before uploading to cloud. Securely store this somewhere as you will need this to restore backup from cloud.
+                                            </div>
+                                        </span>
+                                    </label>
                                     <input id="encryption-key" name="encryption-key" type="password" class="z-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-zinc-700" autocomplete="off" required>
                                 </div>
                                 <div class="flex justify-between space-x-2">
