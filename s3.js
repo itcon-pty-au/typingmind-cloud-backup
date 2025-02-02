a3cc047af3a1dedd0e00b6496bf0e2ca3f6773dd
Thomas <hi@itcon.au>
2025-02-03 08:02:18 +1100

    Parameterize export and import threshold

--- s3.js
+++ s3.js
@@ -1,4 +1,4 @@
-const VERSION = '20250203-07:56';
+const VERSION = '20250203-08:02';
 let backupIntervalRunning = false;
 let wasImportSuccessful = false;
 let isExportInProgress = false;
@@ -253,7 +253,7 @@ function openSyncModal() {
                                     <div class="mt-2 space-y-2">
                                         <div>
                                             <label for="import-threshold" class="block text-sm font-medium text-gray-700 dark:text-gray-400">Import (%)</label>
-                                            <input id="import-threshold" name="import-threshold" type="number" step="0.1" min="0" placeholder="Default: 5" class="z-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-zinc-700" autocomplete="off">
+                                            <input id="import-threshold" name="import-threshold" type="number" step="0.1" min="0" placeholder="Default: 1" class="z-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm dark:bg-zinc-700" autocomplete="off">
                                         </div>
                                         <div>
                                             <label for="export-threshold" class="block text-sm font-medium text-gray-700 dark:text-gray-400">Export (%)</label>
