32217f771bd7094493ed43e8a9eff50ff4121c03
ITCON <itcon.au@gmail.com>
2025-06-15 04:19:04 +1000

    update alert

--- s3-v3.js
+++ s3-v3.js
@@ -3663,7 +3663,7 @@ if (window.typingMindCloudSync) {
               );
               if (success) {
                 alert(
-                  "Backup restored successfully! Please manually reload the page."
+                  "Backup restored successfully! Please manually reload the pageUpdate alert."
                 );
                 // location.reload();
               }
